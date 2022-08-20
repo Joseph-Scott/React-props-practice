5ccae8e0290cca6b9efc188c2eba6cda564e75a7
Joseph Scott <37248298+Joseph-Scott@users.noreply.github.com>
2022-08-20 07:30:32 -0400

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const contacts = [
+  {
+    name: "Bernie",
+    imgURL:
+      "https://scontent-atl3-2.xx.fbcdn.net/v/t39.30808-6/243114763_410304197127074_3183384604965872634_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=frR-YXR_mvEAX88v-5Y&_nc_ht=scontent-atl3-2.xx&oh=00_AT-p6fS7hvgBs9JdspLsRkzp-zbDXEhMDSepapQRRRp6cQ&oe=6304C59E",
+    phone: "+123 456 789",
+    email: "b@bernie.com"
+  },
+  {
+    name: "Alexandria",
+    imgURL:
+      "https://yt3.ggpht.com/ytc/AMLnZu9fceQskq-KJL9xLG3lOsKuqfN_866OdUuhd48JdA=s900-c-k-c0x00ffffff-no-rj",
+    phone: "+987 654 321",
+    email: "alexia@aoc.com"
+  },
+  {
+    name: "Barack",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1329647526807543809/2SGvnHYV_400x400.jpg",
+    phone: "+918 372 574",
+    email: "barack@obama.com"
+  }
+];
+
+export default contacts;
